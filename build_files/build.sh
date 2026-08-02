59f69e231ab6434f182d0c791a1f9fe6b5fd18d8
mightydisciple <nicolasvandyck@proton.me>
2026-08-02 14:06:46 +0200

    noctalia and niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,10 +10,14 @@ cp -avf "/ctx/system_files"/. /
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/44/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+# Keep the custom image deliberately small. Noctalia v5 is packaged in the
+# official Fedora repositories from Fedora 44 onward, so Terra is neither
+# needed nor enabled in the resulting image.
+dnf5 install -y \
+  niri \
+  noctalia
 
 # Use a COPR Example:
 #
